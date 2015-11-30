5dfca79b0757026aa532a18b1f743c5aa8d658ca
Der Mundschenk & Compagnie <github@mundschenk.at>
2015-11-30 10:42:06 +0100

    Starting JavaScript clean-up

--- admin/js/media-credit-preview.js
+++ admin/js/media-credit-preview.js
@@ -1,8 +1,12 @@
-jQuery(document).ready(function($) {
-	$("input[name^='media-credit']").keyup(function (event) {
-		var author = $("span#preview a").clone().wrap('<p>').parent().html();
-		var separator = $("input[name='media-credit[separator]']").val();
-		var organization = $("input[name='media-credit[organization]']").val();
-		$("span#preview").html(author + separator + organization);
-	});
-});
+jQuery( document ).ready( function ( $ ) {
+	
+	$( "input[name^='media-credit']" ).keyup( function ( event ) {
+		
+		var author = $( "span#preview a" ).clone().wrap( '<p>' ).parent().html();
+		var separator = $( "input[name='media-credit[separator]']" ).val();
+		var organization = $( "input[name='media-credit[organization]']" ).val();
+		
+		$( "span#preview" ).html( author + separator + organization );
+	} );
+	
+} );
