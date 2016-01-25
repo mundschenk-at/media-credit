63c3e63ff2ea57f59d1bf63fed798ab6136a8e1a
Der Mundschenk & Compagnie <github@mundschenk.at>
2016-01-25 20:05:00 +0100

    Version bump to 3.0-beta

--- media-credit.php
+++ media-credit.php
@@ -4,7 +4,7 @@
 	Plugin Name: Media Credit
 	Plugin URI: https://code.mundschenk.at/media-credit/
 	Description: This plugin adds a "Credit" field to the media uploading and editing tool and inserts this credit when the images appear on your blog.
-	Version: 3.0-alpha
+	Version: 3.0-beta
 	Author: Peter Putzer
 	Author: Scott Bressler
 	Author URI: https://mundschenk.at/
