b8cf53137426983fe5a057d24044e8eb39a62625
Der Mundschenk & Compagnie <github@mundschenk.at>
2016-07-18 23:13:55 +0200

    Follow WordPress JavaScript Coding Standards

--- admin/js/media-credit-autocomplete.js
+++ admin/js/media-credit-autocomplete.js
@@ -1,50 +1,53 @@
 jQuery( document ).ready( function() {
 	function mediaCreditAutocomplete( inputField, hiddenField, currAuthorId, currAuthor, nonce ) {
-		
+
 		jQuery( inputField ).click( function() {
 			this.select();
 			if ( this.value === currAuthor ) {
 				removeID();
 			}
 		} ).blur( function() {
-			if ( this.value === '' ) {
+			if ( '' === this.value ) {
 				removeID();
 			}
 		} )
-		
+
 		/* --- For jQuery UI autocomplete */
 		.autocomplete( {
-			source : function( request, response ) {
+			source: function( request, response ) {
 				var options = {
-					type : 'POST',
-					data : {
-						'term' : request.term,
-						'limit' : 100,
-						'nonce' : nonce,
+					type: 'POST',
+					data: {
+						term: request.term,
+						limit: 100,
+						nonce: nonce
 					},
-					success : function( data ) {
+					success: function( data ) {
 						response( data );
 					}
 				};
 				wp.media.ajax( 'media_credit_author_names', options );
 			},
-			minLength : 2,
-			select : function( event, ui ) {
+
+			minLength: 2,
+
+			select: function( event, ui ) {
 				addID( ui.item.id );
 				jQuery( inputField ).attr( 'value', ui.item.value ).change();
-				jQuery( hiddenField ).change(); // only needed for property dialog
-				
+				jQuery( hiddenField ).change(); // Only needed for property dialog.
+
 				return false;
 			},
-			open : function() {
+
+			open: function() {
 				jQuery( this ).autocomplete( 'widget' ).css( 'z-index', 2000000 );
-				
+
 				return false;
-			},
+			}
 		} );
 
-		function addID( author_id ) {
-			jQuery( hiddenField ).attr( 'value', author_id );
+		function addID( authorId ) {
+			jQuery( hiddenField ).attr( 'value', authorId );
 		}
 
 		function removeID() {
--- admin/js/media-credit-media-handling.js
+++ admin/js/media-credit-media-handling.js
@@ -1,16 +1,15 @@
 /*
  ** Properly handle editing credits in the media modal.
  */
-
-( function ( $ ) {
+( function( $ ) {
 	/**
 	 * MediaCreditAttachmentModel
-	 * 
+	 *
 	 * @class
 	 * @augments wp.media.model.Attachment
 	 */
 	var MediaCreditAttachmentModel = wp.media.model.Attachment.extend({
-	
+
 		/**
 		 * @param {Object} data The properties to be saved.
 		 * @param {Object} options Sync options. e.g. patch, wait, success, error.
@@ -20,19 +19,18 @@
 		 * @returns {Promise}
 		 */
 		saveCompat: function( data, options ) {
-			var author_id, freeform, image_id, url, nonce, result;
-			
+			var authorId, freeform, imageId, url, nonce, result, ajaxOptions, previousContent, rx, res;
+
 			// Save fields.
 			result = this.constructor.__super__.saveCompat.apply( this, [ data, options ] );
-			
+
 			// Retrieve variables.
-			_.each( data, function ( value, key ) {
-				
-				if ( ! image_id ) {
-					var rx = /attachments\[(\d+)\]/gm,
-						res = rx.exec( key ) || [	'', '0'	];
-					
-					image_id = res[1];
+			_.each( data, function( value, key ) {
+
+				if ( ! imageId ) {
+					rx = /attachments\[(\d+)\]/gm;
+					res = rx.exec( key ) || [ '', '0' ];
+					imageId = res[1];
 				}
 
 				if ( ! freeform && key.match( /attachments\[\d+\]\[media-credit\]/gm ) ) {
@@ -43,49 +41,49 @@
 					url = value;
 				}
 
-				if ( ! author_id && key.match( /attachments\[\d+\]\[media-credit-hidden\]/gm ) ) {
-					author_id = value;
+				if ( ! authorId && key.match( /attachments\[\d+\]\[media-credit-hidden\]/gm ) ) {
+					authorId = value;
 				}
 			} );
-			
+
 			// Update content.
-			var previous_content = jQuery( '#content' ).val();
-			
-			if ( previous_content ) {
-				var ajax_options = {
-					type : 'POST',
-					data : {
-						'author_id'    : author_id,
-						'freeform'     : freeform,
-						'post_content' : previous_content,
-						'image_id'     : image_id,
-						'url'          : url,
-						'nonce'        : this.get('nonces').update,
+			previousContent = jQuery( '#content' ).val();
+
+			if ( previousContent ) {
+				ajaxOptions = {
+					type: 'POST',
+					data: {
+						'author_id':    authorId,
+						'freeform':     freeform,
+						'post_content': previousContent,
+						'image_id':     imageId,
+						'url':          url,
+						'nonce':        this.get( 'nonces' ).update
 					},
-					success : function ( new_content ) {
+					success: function( newContent ) {
 						var editor;
-						
-						if ( previous_content === new_content ) {
-							return; // nothing has changed
+
+						if ( previousContent === newContent ) {
+							return; // Nothing has changed.
 						}
 
 						editor = tinymce.get( 'content' );
 						if ( editor && editor instanceof tinymce.Editor && jQuery( '#wp-content-wrap' ).hasClass( 'tmce-active' ) ) {
-							editor.setContent( new_content );
-							editor.save( { no_events : true } );
+							editor.setContent( newContent );
+							editor.save( { no_events: true } );
 						} else {
-							jQuery( 'textarea#content' ).val( new_content );
+							jQuery( 'textarea#content' ).val( newContent );
 						}
 					}
 				};
-				
-				wp.media.ajax( 'media_credit_filter_content', ajax_options );
+
+				wp.media.ajax( 'media_credit_filter_content', ajaxOptions );
 			}
-			
+
 			return result;
-		},
+		}
 	} );
 
 	wp.media.model.Attachment.prototype = MediaCreditAttachmentModel.prototype;
-	
+
 } )( jQuery );
