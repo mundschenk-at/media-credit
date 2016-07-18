717937f6a8283e4c70303f5caf5f3ea9e25781e2
Der Mundschenk & Compagnie <github@mundschenk.at>
2016-07-18 23:25:06 +0200

    JS coding standards

--- admin/js/tinymce4/media-credit-image-properties.js
+++ admin/js/tinymce4/media-credit-image-properties.js
@@ -1,4 +1,4 @@
-(function( $, wp, _ ) {
+( function( $, wp, _ ) {
 	var MediaCreditImagePropertiesView, frame;
 
 	if ( ! wp.media.events ) {
@@ -9,13 +9,13 @@
 		var mediaCreditView = new MediaCreditImagePropertiesView( { model: view.model } );
 
 		view.on( 'post-render', function() {
-			view.views.insert( view.$el.find('.advanced-image'), mediaCreditView.render().el );
+			view.views.insert( view.$el.find( '.advanced-image' ), mediaCreditView.render().el );
 		} );
 	}
 
-	wp.media.events.on( 'editor:image-edit', function( options ) {	
-		if (options.metadata.mediaCreditName === '' && options.metadata.mediaCreditID !== '') {
-			options.metadata.mediaCreditName = $mediaCredit['id'][options.metadata.mediaCreditID];
+	wp.media.events.on( 'editor:image-edit', function( options ) {
+		if (  '' === options.metadata.mediaCreditName && '' !== options.metadata.mediaCreditID ) {
+			options.metadata.mediaCreditName = $mediaCredit.id[options.metadata.mediaCreditID];
 		}
 	} );
 
@@ -24,77 +24,81 @@
 		frame.on( 'content:render:image-details', addMediaCreditView );
 	} );
 
-	wp.media.events.on( 'editor:image-update', function( options ) {	
+	wp.media.events.on( 'editor:image-update', function( options ) {
 		var editor = options.editor,
 			dom = editor.dom,
 			image  = options.image,
 			model = frame.content.get().model,
-			align = model.get('align'),
-			width = model.get('width'),
+			align = model.get( 'align' ),
+			width = model.get( 'width' ),
 			credit,
-			mediaCreditName = model.get('mediaCreditName'),
-			mediaCreditID = model.get('mediaCreditID'),
-			mediaCreditLink = model.get('mediaCreditLink'),
+			mediaCreditName = model.get( 'mediaCreditName' ),
+			mediaCreditID = model.get( 'mediaCreditID' ),
+			mediaCreditLink = model.get( 'mediaCreditLink' ),
 			mediaCreditBlock,
 			mediaCreditWrapper;
 
-		/* Extract mediaCreditBlock in visual editor */
-		if ( image.parentNode && image.parentNode.nodeName === 'A' ) {
-			mediaCreditBlock = dom.getNext( image.parentNode, '.mceMediaCreditTemp' );			
+		// Extract mediaCreditBlock in visual editor.
+		if ( image.parentNode && 'A' === image.parentNode.nodeName ) {
+			mediaCreditBlock = dom.getNext( image.parentNode, '.mceMediaCreditTemp' );
 		} else {
 			mediaCreditBlock = dom.getNext( image, '.mceMediaCreditTemp' );;
 		}
-		
+
 		if ( $mediaCredit.id[mediaCreditID] !== mediaCreditName ) {
 			mediaCreditID = '';
 		}
 
-		credit = mediaCreditID ? ($mediaCredit.id[mediaCreditID] + $mediaCredit.separator + $mediaCredit.organization) : mediaCreditName;
-		credit = credit.replace(/<[^>]+>(.*)<\/[^>]+>/g, '$1'); // basic sanitation
-		align = 'align' + ( align || 'none' ); 
-		
-		// no current media block
-		if ( mediaCreditBlock === null && ( mediaCreditName || mediaCreditID ) ) {			
-			// create new representation for media-credit
-			mediaCreditBlock = dom.create ('span', {
-												'class': 'mceMediaCreditTemp mceNonEditable',
-												'data-media-credit-id': mediaCreditID,
-												'data-media-credit-name': mediaCreditName,
+		credit = mediaCreditID ? ( $mediaCredit.id[mediaCreditID] + $mediaCredit.separator + $mediaCredit.organization ) : mediaCreditName;
+		credit = credit.replace( /<[^>]+>(.*)<\/[^>]+>/g, '$1' ); // Basic sanitation.
+		align = 'align' + ( align || 'none' );
+
+		// No current media block.
+		if ( null === mediaCreditBlock && ( mediaCreditName || mediaCreditID ) ) {
+
+			// Create new representation for media-credit.
+			mediaCreditBlock = dom.create( 'span', {
+												'class':                  'mceMediaCreditTemp mceNonEditable',
+												'data-media-credit-id':    mediaCreditID,
+												'data-media-credit-name':  mediaCreditName,
 												'data-media-credit-align': align,
-												'data-media-credit-link' : mediaCreditLink
-											}, credit);
-			
-			if ( image.parentNode && image.parentNode.nodeName === 'A' ) {
+												'data-media-credit-link':  mediaCreditLink
+										 }, credit );
+
+			if ( image.parentNode && 'A' === image.parentNode.nodeName ) {
 				dom.insertAfter( mediaCreditBlock, image.parentNode );
 			} else {
 				dom.insertAfter( mediaCreditBlock, image );
 			}
 		}
-		
+
 		if ( mediaCreditBlock ) {
-			
-			// check for media-credit nested inside caption
-			if ( !dom.getParent( mediaCreditBlock, 'dl.wp-caption' ) ) {
-				// standalone [media-credit]
-				mediaCreditWrapper = dom.create( 'div', { 'class': 'mceMediaCreditOuterTemp ' + align,
-					  									  'style': 'width: ' + (parseInt(width) + 10) + 'px' } );
-			
-				// swap existing parent with our new wrapper
-				dom.insertAfter(mediaCreditWrapper, mediaCreditBlock.parentNode);
-				dom.add(mediaCreditWrapper, mediaCreditBlock.parentNode);
-				dom.remove(mediaCreditBlock.parentNode, true);
-			}		
-					
-			dom.setAttrib(mediaCreditBlock, 'data-media-credit-name', mediaCreditName);
-			dom.setAttrib(mediaCreditBlock, 'data-media-credit-id', mediaCreditID);
-			dom.setAttrib(mediaCreditBlock, 'data-media-credit-link', mediaCreditLink);
-			dom.setHTML(mediaCreditBlock, credit);
+
+			// Check for media-credit nested inside caption.
+			if ( ! dom.getParent( mediaCreditBlock, 'dl.wp-caption' ) ) {
+
+				// Standalone [media-credit].
+				mediaCreditWrapper = dom.create( 'div', {
+					'class': 'mceMediaCreditOuterTemp ' + align,
+					'style': 'width: ' + ( parseInt( width ) + 10 ) + 'px'
+				} );
+
+				// Swap existing parent with our new wrapper.
+				dom.insertAfter( mediaCreditWrapper, mediaCreditBlock.parentNode );
+				dom.add( mediaCreditWrapper, mediaCreditBlock.parentNode );
+				dom.remove( mediaCreditBlock.parentNode, true );
+			}
+
+			dom.setAttrib( mediaCreditBlock, 'data-media-credit-name', mediaCreditName );
+			dom.setAttrib( mediaCreditBlock, 'data-media-credit-id', mediaCreditID );
+			dom.setAttrib( mediaCreditBlock, 'data-media-credit-link', mediaCreditLink );
+			dom.setHTML( mediaCreditBlock, credit );
 		}
 	} );
 
 	MediaCreditImagePropertiesView = wp.Backbone.View.extend( {
 		className: 'media-credit-image-properties',
-		template: wp.media.template('media-credit-image-properties'),
+		template: wp.media.template( 'media-credit-image-properties' ),
 
 		initialize: function() {
 			wp.Backbone.View.prototype.initialize.apply( this, arguments );
@@ -102,8 +106,6 @@
 
 		prepare: function() {
 			var data = this.model.toJSON();
-			//data.mediaCreditName = "foobar";
-			//data.mediaCreditID = "-1";
 			return data;
 		},
 
@@ -113,4 +115,4 @@
 		}
 	} );
 
-})( jQuery, wp, _ );
+} )( jQuery, wp, _ );
