f5af0ee60adbbd6534972feddcc30c0b692ef778
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-01-27 19:11:05 +0100

    Grunt: use grunt-newer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,216 +1,218 @@
 'use strict';
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
 	// load all tasks
-	require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+	require( 'load-grunt-tasks' )( grunt, { scope: 'devDependencies' } );
 
-    grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-	    makepot: {
-	        target: {
-	            options: {
-	                domainPath: '/translations/', // Where to save the POT file.
-	                potFilename: 'media-credit.pot', // Name of the POT file.
-	                type: 'wp-plugin',
-	                exclude: ['build/.*'],
-	                updateTimestamp: false
-	            }
-	        }
-	    },
-//	    phpunit: {
-//	        classes: {
-//	            options: {
-//	            	testsuite: 'media-credit',
-//	            }
-//	        },
-//	        options: {
-//	            colors: true,
-//	            configuration: 'phpunit.xml',
-//	        }
-//	    }
-	    clean: {
-	    	  build: ["build/*"]//,
-    	},
-	    wp_readme_to_markdown: {
-	        readme: {
-	            files: {
-	              'README.md': 'readme.txt',
-	            },
-  	        },
-  	        options: {
-  	        	screenshot_url: 'wp-assets/{screenshot}.png',
-  	        }
-	    },
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( 'package.json' ),
+//		phpunit: {
+//		classes: {
+//		options: {
+//		testsuite: 'media-credit',
+//		}
+//		},
+//		options: {
+//		colors: true,
+//		configuration: 'phpunit.xml',
+//		}
+//		}
+		clean: {
+			build: [ "build/*" ]//,
+		},
+		wp_readme_to_markdown: {
+			readme: {
+				files: {
+					'README.md': 'readme.txt',
+				},
+			},
+			options: {
+				screenshot_url: 'wp-assets/{screenshot}.png',
+			}
+		},
 		copy: {
-			main: {
-				files:[
-					{expand: true, nonull: true, src: ['readme.txt','*.php'], dest: 'build/'},
-					{expand: true, nonull: true, src: ['admin/**','public/**','includes/**', '!**/scss/**'], dest: 'build/'},
+			build: {
+				files: [
+					{ expand: true, nonull: true, src: ['readme.txt','*.php'], dest: 'build/' },
+					{ expand: true, nonull: true, src: ['admin/**','public/**','includes/**', '!**/scss/**'], dest: 'build/' },
 				],
 			}
 		},
 
-	    wp_deploy: {
-            options: {
-                plugin_slug: 'media-credit',
-                // svn_user: 'your-wp-repo-username',
-                build_dir: 'build', //relative path to your build directory
-                assets_dir: 'wp-assets', //relative path to your assets directory (optional).
-                max_buffer: 1024 * 1024
-            },
-            release: {
-            	// nothing
-	        },
-	        trunk: {
-	        	options: {
-	            	deploy_trunk: true,
-	            	deploy_assets: true,
-	            	deploy_release: false,
-	        	}
-            },
-            assets: {
-            	options: {
-            		deploy_assets: true,
-            		deploy_trunk: false,
-            		deploy_release: false,
-            	}
-            }
-	    },
+		wp_deploy: {
+			options: {
+				plugin_slug: 'media-credit',
+				// svn_user: 'your-wp-repo-username',
+				build_dir: 'build', //relative path to your build directory
+				assets_dir: 'wp-assets', //relative path to your assets directory (optional).
+				max_buffer: 1024 * 1024
+			},
+			release: {
+				// nothing
+			},
+			trunk: {
+				options: {
+					deploy_trunk: true,
+					deploy_assets: true,
+					deploy_release: false,
+				}
+			},
+			assets: {
+				options: {
+					deploy_assets: true,
+					deploy_trunk: false,
+					deploy_release: false,
+				}
+			}
+		},
 
-	    jshint: {
-            files: [
-                'admin/js/**/*.js',
-                'public/js/**/*.js'
-            ],
-            options: {
-                expr: true,
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true,
-                    document: true
-                }
-            }
-        },
+		jshint: {
+			files: [
+				'admin/js/**/*.js',
+				'public/js/**/*.js'
+			],
+			options: {
+				expr: true,
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true,
+					document: true
+				}
+			}
+		},
 
-        jscs: {
-            src: [
-                'admin/js/**/*.js',
-                'public/js/**/*.js'
-            ],
-            options: {
-            }
-        },
+		jscs: {
+			src: [
+				'admin/js/**/*.js',
+				'public/js/**/*.js'
+			],
+			options: {
+			}
+		},
 
-	    phpcs: {
-	        plugin: {
-	            src: ['includes/**/*.php', 'admin/**/*.php', 'public/**/*.php']
-	        },
-	        options: {
-	        	bin: 'phpcs -p -s -v -n ',
-	            standard: './codesniffer.ruleset.xml'
-	        }
-	    },
+		phpcs: {
+			plugin: {
+				src: ['includes/**/*.php', 'admin/**/*.php', 'public/**/*.php']
+			},
+			options: {
+				bin: 'phpcs -p -s -v -n ',
+				standard: './codesniffer.ruleset.xml'
+			}
+		},
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed',
-                    sourcemap: 'none',
-                    compass: true
-                },
-                files: [ { expand: true,
-		                   cwd: 'admin/scss',
-		                   src: [ '**/*.scss' ],
-		                   dest: 'build/admin/css',
-		                   ext: '.min.css' },
-	                     { expand: true,
-		                   cwd: 'public/scss',
-		                   src: [ '**/*.scss' ],
-		                   dest: 'build/public/css',
-		                   ext: '.min.css' } ]
-            },
-            dev: {
-                options: {
-                    style: 'expanded',
-                    sourcemap: 'none',
-                    compass: true
-                },
-                files: [ { expand: true,
-		                   cwd: 'admin/scss',
-		                   src: [ '**/*.scss' ],
-		                   dest: 'admin/css',
-		                   ext: '.css' },
-	                     { expand: true,
-		                   cwd: 'public/scss',
-		                   src: [ '**/*.scss' ],
-		                   dest: 'public/css',
-		                   ext: '.css' } ]
-            }
-        },
-        uglify: {
-            dist: {
-                options: {
-                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-                    report: 'gzip'
-                },
-                files: grunt.file.expandMapping(['admin/js/**/*.js', 'public/js/**/*.js'], 'build/', {
-                    rename: function(destBase, destPath) {
-                        return destBase+destPath.replace('.js', '.min.js');
-                    }
-                })
-            },
-//            dev: {
-//                options: {
-//                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-//                    beautify: true,
-//                    compress: false,
-//                    mangle: false
-//                },
-//                files: {
-//                    'assets/js/filename.js' : [
-//                        'assets/path/to/file.js',
-//                        'assets/path/to/another/file.js',
-//                        'assets/dynamic/paths/**/*.js'
-//                    ]
-//                }
-//            }
-        }
+		delegate: {
+			sass: {
+				src: [ '<%= sass.dev.files.src %>**/*.scss' ],
+				dest: '<%= sass.dev.files.dest %>'
+			}
+		},
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'none',
+					compass: true
+				},
+				files: [ {
+					expand: true,
+					cwd: 'admin/scss',
+					src: [ '**/*.scss' ],
+					dest: 'build/admin/css',
+					ext: '.min.css'
+				},
+				{
+					expand: true,
+					cwd: 'public/scss',
+					src: [ '**/*.scss' ],
+					dest: 'build/public/css',
+					ext: '.min.css'
+				} ]
+			},
+			dev: {
+				options: {
+					style: 'expanded',
+					sourcemap: 'none',
+					compass: true
+				},
+				files: [ {
+					expand: true,
+					cwd: 'admin/scss',
+					src: [ '**/*.scss' ],
+					dest: 'admin/css',
+					ext: '.css'
+				},
+				{
+					expand: true,
+					cwd: 'public/scss',
+					src: [ '**/*.scss' ],
+					dest: 'public/css',
+					ext: '.css'
+				} ]
+			}
+		},
+
+		// uglify targets are dynamically generated by the minify task
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= ugtargets[grunt.task.current.target].filename %> <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+				report: 'min',
+			},
+		},
+
+		minify: {
+			dist: {
+				files: grunt.file.expandMapping( [ 'admin/js/**/*.js', '!admin/js/**/*min.js', 'public/js/**/*.js', '!public/js/**/*min.js' ], '', {
+					rename: function(destBase, destPath) {
+						return destBase+destPath.replace('.js', '.min.js');
+					}
+				})
+			},
+		},
 	});
 
 	grunt.registerTask( 'default', [
-	    'wp_readme_to_markdown',
-		//'makepot',
-		'sass:dev'
-    ]);
+			'wp_readme_to_markdown',
+			'newer:jscs',
+			'newer:jshint',
+			'newer:phpcs',
+			'newer:delegate:sass:dev'
+	] );
 
 	grunt.registerTask( 'build', [
-		'wp_readme_to_markdown',
-		'clean:build',
-		'copy',
-		'sass:dist',
-		'uglify:dist'
-  	]);
+			'wp_readme_to_markdown',
+			'clean:build',
+			'newer:delegate:sass:build',
+			'newer:minify',
+			'copy:build'
+	] );
 
-  	grunt.registerTask('deploy', [
- 	    //'phpunit:default',
- 	    'phpcs',
- 	    'jscs',
-		'build',
-  		'wp_deploy:release'
-  	]);
-  	grunt.registerTask('trunk', [
- 		//'wp_readme_to_markdown',
- 	    //'phpunit:default',
-    	'phpcs',
- 	    'jscs',
-    	'build',
-  		'wp_deploy:trunk'
-  	]);
-  	grunt.registerTask('assets', [
-		'clean:build',
-  		'copy',
-  		'wp_deploy:assets'
-	]);
+	// delegate stuff
+	grunt.registerTask( 'delegate', function() {
+		grunt.task.run( this.args.join( ':' ) );
+	} );
+
+	grunt.registerTask('deploy', [
+			'phpcs',
+			'jshint',
+			'jscs',
+			'build',
+			'wp_deploy:release'
+	] );
+
+	grunt.registerTask('trunk', [
+			'wp_readme_to_markdown',
+			'phpcs',
+			'jscs',
+			'build',
+			'wp_deploy:trunk'
+	] );
+
+	grunt.registerTask('assets', [
+			'clean:build',
+			'copy',
+			'wp_deploy:assets'
+	] );
 
 };
