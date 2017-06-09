3af3d95af16e594322a02bcb43873556ce5958f7
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-06-09 20:59:14 +0200

    Update to SVN@40786

--- admin/js/tinymce4/media-credit-tinymce.js
+++ admin/js/tinymce4/media-credit-tinymce.js
@@ -659,7 +659,6 @@ tinymce.PluginManager.add( 'mediacredit', function( editor ) {
 		// Preserve empty alt attributes.
 		editor.$( imageNode ).attr( 'alt', imageData.alt || '' );
 
-
 		linkAttrs = {
 			href: imageData.linkUrl,
 			rel: imageData.linkRel || null,
@@ -1236,6 +1235,14 @@ tinymce.PluginManager.add( 'mediacredit', function( editor ) {
 					p = dom.create( 'p' );
 					dom.insertAfter( p, captionParent );
 					editor.selection.setCursorLocation( p, 0 );
+
+					// If the image is selected and the user pastes "over" it,
+					// replace both the image and the caption elements with the pasted content.
+					// This matches the behavior when pasting over non-caption images.
+					if ( node.nodeName === 'IMG' ) {
+						editor.$( captionParent ).remove();
+					}
+
 					editor.nodeChanged();
 				} else {
 					// Clicking Indent or Outdent while an image with a caption is selected breaks the caption.
