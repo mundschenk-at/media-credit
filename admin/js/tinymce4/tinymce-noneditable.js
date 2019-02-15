fa0acbe80c77d8ac07351a0ea522f61a17c1f18a
Peter Putzer <github@mundschenk.at>
2019-02-15 07:56:46 +0100

    Using the upstream noneditable plugin is not so easy anymore, so let's clean up this mess

--- admin/js/tinymce4/tinymce-noneditable.js
+++ admin/js/tinymce4/tinymce-noneditable.js
@@ -8,33 +8,33 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
-(function ( tinymce ) {
+( function( tinymce ) {
 'use strict';
 
 var defs = {}; // id -> {dependencies, definition, instance (possibly undefined)}
 
-var instantiate = function (id) {
+var instantiate = function( id ) {
   var actual = defs[id];
   var dependencies = actual.deps;
   var definition = actual.defn;
   var len = dependencies.length;
-  var instances = new Array(len);
-  for (var i = 0; i < len; ++i) {
-    instances[i] = dem(dependencies[i]);
+  var instances = new Array( len );
+  var defResult = definition.apply( null, instances );
+  for ( var i = 0; i < len; ++i ) { // eslint-disable-line vars-on-top
+    instances[i] = dem( dependencies[i] );
   }
-  var defResult = definition.apply(null, instances);
-  if (defResult === undefined) {
+  if ( defResult === undefined ) {
      throw 'module [' + id + '] returned undefined';
   }
   actual.instance = defResult;
 };
 
-var def = function (id, dependencies, definition) {
-  if (typeof id !== 'string') {
+var def = function( id, dependencies, definition ) {
+  if ( 'string' !== typeof id ) {
     throw 'module id must be a string';
-  } else if (dependencies === undefined) {
+  } else if ( dependencies === undefined ) {
     throw 'no dependencies for ' + id;
-  } else if (definition === undefined) {
+  } else if ( definition === undefined ) {
     throw 'no definition function for ' + id;
   }
   defs[id] = {
@@ -44,23 +44,23 @@ var def = function (id, dependencies, definition) {
   };
 };
 
-var dem = function (id) {
+var dem = function( id ) {
   var actual = defs[id];
-  if (actual === undefined) {
+  if ( actual === undefined ) {
     throw 'module [' + id + '] was undefined';
-  } else if (actual.instance === undefined) {
-    instantiate(id);
+  } else if ( actual.instance === undefined ) {
+    instantiate( id );
   }
   return actual.instance;
 };
 
-var req = function (ids, callback) {
+var req = function( ids, callback ) {
   var len = ids.length;
-  var instances = new Array(len);
-  for (var i = 0; i < len; ++i) {
-    instances.push(dem(ids[i]));
+  var instances = new Array( len );
+  for ( var i = 0; i < len; ++i ) { // eslint-disable-line vars-on-top
+    instances.push( dem( ids[i] ) );
   }
-  callback.apply(null, callback);
+  callback.apply( null, callback );
 };
 
 var ephox = {};
@@ -75,15 +75,20 @@ ephox.bolt = {
   }
 };
 
-var define = def;
+var define = def; // eslint-disable-line vars-on-top
+
 // this helps with minificiation when using a lot of global references
-var defineGlobal = function (id, ref) {
-  define(id, [], function () { return ref; });
+var defineGlobal = function( id, ref ) { // eslint-disable-line vars-on-top
+  define( id, [], function() {
+ return ref;
+} );
 };
+
 /*jsc
 ["tinymce.plugins.noneditable.Plugin","tinymce.core.PluginManager","tinymce.core.util.Tools","global!tinymce.util.Tools.resolve"]
 jsc*/
-defineGlobal('global!tinymce.util.Tools.resolve', tinymce.util.Tools.resolve);
+defineGlobal( 'global!tinymce.util.Tools.resolve', tinymce.util.Tools.resolve );
+
 /**
  * ResolveGlobal.js
  *
@@ -99,8 +104,8 @@ define(
   [
     'global!tinymce.util.Tools.resolve'
   ],
-  function (resolve) {
-    return resolve('tinymce.PluginManager');
+  function( resolve ) {
+    return resolve( 'tinymce.PluginManager' );
   }
 );
 
@@ -119,8 +124,8 @@ define(
   [
     'global!tinymce.util.Tools.resolve'
   ],
-  function (resolve) {
-    return resolve('tinymce.util.Tools');
+  function( resolve ) {
+    return resolve( 'tinymce.util.Tools' );
   }
 );
 
@@ -146,111 +151,117 @@ define(
     'tinymce.core.PluginManager',
     'tinymce.core.util.Tools'
   ],
-  function (PluginManager, Tools) {
-    PluginManager.add('noneditable', function (editor) {
-      var editClass, nonEditClass, nonEditableRegExps, contentEditableAttrName = 'contenteditable';
+  function( PluginManager, Tools ) {
+    PluginManager.add( 'noneditable', function( editor ) {
+      var editClass, nonEditClass, nonEditableRegExps,
+          contentEditableAttrName = 'contenteditable';
 
-      function hasClass(checkClassName) {
-        return function (node) {
-          return (' ' + node.attr('class') + ' ').indexOf(checkClassName) !== -1;
+      function hasClass( checkClassName ) {
+        return function( node ) {
+          return -1 !== ( ' ' + node.attr( 'class' ) + ' ' ).indexOf( checkClassName );
         };
       }
 
-      function convertRegExpsToNonEditable(e) {
-        var i = nonEditableRegExps.length, content = e.content, cls = Tools.trim(nonEditClass);
+      function convertRegExpsToNonEditable( e ) {
+        var i = nonEditableRegExps.length,
+            content = e.content,
+            cls = Tools.trim( nonEditClass );
 
-        function replaceMatchWithSpan(match) {
-          var args = arguments, index = args[args.length - 2];
-          var prevChar = index > 0 ? content.charAt(index - 1) : '';
+        function replaceMatchWithSpan( match ) {
+          var args = arguments,
+              index = args[args.length - 2];
+          var prevChar = 0 < index ? content.charAt( index - 1 ) : '';
 
           // Is value inside an attribute then don't replace
-          if (prevChar === '"') {
+          if ( '"' === prevChar ) {
             return match;
           }
 
           // Is value inside a contentEditable="false" tag
-          if (prevChar === '>') {
-            var findStartTagIndex = content.lastIndexOf('<', index);
-            if (findStartTagIndex !== -1) {
-              var tagHtml = content.substring(findStartTagIndex, index);
-              if (tagHtml.indexOf('contenteditable="false"') !== -1) {
+          if ( '>' === prevChar ) {
+            var findStartTagIndex = content.lastIndexOf( '<', index ); // eslint-disable-line vars-on-top
+            if ( -1 !== findStartTagIndex ) {
+              var tagHtml = content.substring( findStartTagIndex, index ); // eslint-disable-line vars-on-top
+              if ( -1 !== tagHtml.indexOf( 'contenteditable="false"' ) ) {
                 return match;
               }
             }
           }
 
           return (
-            '<span class="' + cls + '" data-mce-content="' + editor.dom.encode(args[0]) + '">' +
-            editor.dom.encode(typeof args[1] === 'string' ? args[1] : args[0]) + '</span>'
+            '<span class="' + cls + '" data-mce-content="' + editor.dom.encode( args[0] ) + '">' +
+            editor.dom.encode( 'string' === typeof args[1] ? args[1] : args[0] ) + '</span>'
           );
         }
 
         // Don't replace the variables when raw is used for example on undo/redo
-        if ( e.format === 'raw' ) {
+        if ( 'raw' === e.format ) {
           return;
         }
 
-        while (i--) {
-          content = content.replace(nonEditableRegExps[i], replaceMatchWithSpan);
+        while ( i-- ) {
+          content = content.replace( nonEditableRegExps[i], replaceMatchWithSpan );
         }
 
         e.content = content;
       }
 
-      editClass = ' ' + Tools.trim(editor.getParam('noneditable_editable_class', 'mceEditable')) + ' ';
-      nonEditClass = ' ' + Tools.trim(editor.getParam('noneditable_noneditable_class', 'mceNonEditable')) + ' ';
+      editClass = ' ' + Tools.trim( editor.getParam( 'noneditable_editable_class', 'mceEditable' ) ) + ' ';
+      nonEditClass = ' ' + Tools.trim( editor.getParam( 'noneditable_noneditable_class', 'mceNonEditable' ) ) + ' ';
 
-      var hasEditClass = hasClass(editClass);
-      var hasNonEditClass = hasClass(nonEditClass);
+      var hasEditClass = hasClass( editClass ); // eslint-disable-line vars-on-top
+      var hasNonEditClass = hasClass( nonEditClass ); // eslint-disable-line vars-on-top
 
-      nonEditableRegExps = editor.getParam('noneditable_regexp');
-      if (nonEditableRegExps && !nonEditableRegExps.length) {
-        nonEditableRegExps = [nonEditableRegExps];
+      nonEditableRegExps = editor.getParam( 'noneditable_regexp' );
+      if ( nonEditableRegExps && ! nonEditableRegExps.length ) {
+        nonEditableRegExps = [ nonEditableRegExps ];
       }
 
-      editor.on('PreInit', function () {
-        if (nonEditableRegExps) {
-          editor.on('BeforeSetContent', convertRegExpsToNonEditable);
+      editor.on( 'PreInit', function() {
+        if ( nonEditableRegExps ) {
+          editor.on( 'BeforeSetContent', convertRegExpsToNonEditable );
         }
 
-        editor.parser.addAttributeFilter('class', function (nodes) {
-          var i = nodes.length, node;
+        editor.parser.addAttributeFilter( 'class', function( nodes ) {
+          var i = nodes.length,
+              node;
 
-          while (i--) {
+          while ( i-- ) {
             node = nodes[i];
 
-            if (hasEditClass(node)) {
-              node.attr(contentEditableAttrName, 'true');
-            } else if (hasNonEditClass(node)) {
-              node.attr(contentEditableAttrName, 'false');
+            if ( hasEditClass( node ) ) {
+              node.attr( contentEditableAttrName, 'true' );
+            } else if ( hasNonEditClass( node ) ) {
+              node.attr( contentEditableAttrName, 'false' );
             }
           }
-        });
+        } );
 
-        editor.serializer.addAttributeFilter(contentEditableAttrName, function (nodes) {
-          var i = nodes.length, node;
+        editor.serializer.addAttributeFilter( contentEditableAttrName, function( nodes ) {
+          var i = nodes.length,
+              node;
 
-          while (i--) {
+          while ( i-- ) {
             node = nodes[i];
-            if (!hasEditClass(node) && !hasNonEditClass(node)) {
+            if ( ! hasEditClass( node ) && ! hasNonEditClass( node ) ) {
               continue;
             }
 
-            if (nonEditableRegExps && node.attr('data-mce-content')) {
+            if ( nonEditableRegExps && node.attr( 'data-mce-content' ) ) {
               node.name = '#text';
               node.type = 3;
               node.raw = true;
-              node.value = node.attr('data-mce-content');
+              node.value = node.attr( 'data-mce-content' );
             } else {
-              node.attr(contentEditableAttrName, null);
+              node.attr( contentEditableAttrName, null );
             }
           }
-        });
-      });
-    });
+        } );
+      } );
+    } );
 
-    return function () { };
+    return function() { };
   }
 );
-dem('tinymce.plugins.noneditable.Plugin')();
-})( window.tinymce );
+dem( 'tinymce.plugins.noneditable.Plugin' )();
+} ( window.tinymce ) );
