0749bd0fbcf069203d1e9830a866dba50d504c8d
Peter Putzer <github@mundschenk.at>
2019-03-11 11:25:53 +0100

    Meta: Add CHANGELOG and LICENSE to build files

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,8 @@ module.exports = function( grunt ) {
 					nonull: true,
 					src: [
 						'readme.txt',
+						'CHANGELOG.md',
+						'LICENSE.md',
 						'*.php',
 						'admin/**',
 						'public/**',
