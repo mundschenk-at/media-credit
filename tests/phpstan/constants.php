17f6c03f007cf3f771438718b2deba5464ea8185
mundschenk_at <github@mundschenk.at>
2021-03-14 16:02:03 +0100

    Meta: Add PHPStan

---
+++ tests/phpstan/constants.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of Media Credit.
+ *
+ * Copyright 2021 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  ***
+ *
+ * @package mundschenk-at/media-credit/tests
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
+
+// Avatar Privacy constants.
+define( 'MEDIA_CREDIT_PLUGIN_FILE', '/some/path/file' );
+define( 'MEDIA_CREDIT_PLUGIN_PATH', '/some/path' );
+
+// WordPress Core constants.
+define( 'COOKIE_DOMAIN', 'example.org' );
+define( 'COOKIEHASH', 'b7d8d5a26884619e3b3b4481ba778642' );
+define( 'COOKIEPATH', '/some/path' );
