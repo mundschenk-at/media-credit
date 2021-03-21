07258e51cff42da71d3673872d147332c8d6dc5a
mundschenk_at <github@mundschenk.at>
2021-03-21 20:43:49 +0100

    Tests: Add unit test for Settings

---
+++ tests/Media_Credit/Settings_Test.php
@@ -0,0 +1,456 @@
+<?php
+/**
+ * This file is part of Media Credit.
+ *
+ * Copyright 2021 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  ***
+ *
+ * @package mundschenk-at/media-credit/tests
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Media_Credit\Tests;
+
+use Brain\Monkey\Actions;
+use Brain\Monkey\Filters;
+use Brain\Monkey\Functions;
+
+use Mockery as m;
+
+use Media_Credit\Settings;
+
+use Media_Credit\Data_Storage\Options;
+
+/**
+ * Media_Credit\Settings unit test.
+ *
+ * @coversDefaultClass \Media_Credit\Settings
+ * @usesDefaultClass \Media_Credit\Settings
+ *
+ * @uses ::__construct
+ */
+class Settings_Test extends \Media_Credit\Tests\TestCase {
+
+	const VERSION = '1.0.0';
+
+	/**
+	 * The system-under-test.
+	 *
+	 * @var Settings
+	 */
+	private $sut;
+
+	/**
+	 * Required helper object.
+	 *
+	 * @var Options
+	 */
+	private $options;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function set_up() {
+		parent::set_up();
+
+		Functions\when( '__' )->returnArg();
+
+		$this->options = m::mock( Options::class );
+
+		$this->sut = m::mock( Settings::class, [ self::VERSION, $this->options ] )
+			->makePartial()
+			->shouldAllowMockingProtectedMethods();
+	}
+
+	/**
+	 * Test ::__construct.
+	 *
+	 * @covers ::__construct
+	 *
+	 * @uses ::get_version
+	 */
+	public function test_constructor() {
+		$version = '6.6.6';
+		$options = m::mock( Options::class );
+
+		$sut = m::mock( Settings::class )->makePartial();
+		$sut->__construct( $version, $options );
+
+		$this->assert_attribute_same( $version, 'version', $sut );
+		$this->assert_attribute_same( $options, 'options', $sut );
+	}
+
+	/**
+	 * Tests ::get_version.
+	 *
+	 * @covers ::get_version
+	 */
+	public function test_get_version() {
+		$this->assertSame( self::VERSION, $this->sut->get_version() );
+	}
+
+	/**
+	 * Tests ::get_all_settings.
+	 *
+	 * @covers ::get_all_settings
+	 */
+	public function test_get_all_settings() {
+		$settings = [
+			'foo'                       => 'bar',
+			Settings::INSTALLED_VERSION => self::VERSION,
+		];
+
+		$this->sut->shouldReceive( 'load_settings' )->once()->andReturn( $settings );
+
+		$this->assertSame( $settings, $this->sut->get_all_settings() );
+	}
+
+	/**
+	 * Tests ::get_all_settings.
+	 *
+	 * @covers ::get_all_settings
+	 */
+	public function test_get_all_settings_repeated() {
+		$original = [
+			'foo'                       => 'bar',
+			Settings::INSTALLED_VERSION => self::VERSION,
+		];
+
+		// Prepare state - settings have alreaddy been loaded.
+		$this->set_value( $this->sut, 'settings', $original );
+
+		$this->sut->shouldReceive( 'load_settings' )->never();
+
+		$this->assertSame( $original, $this->sut->get_all_settings() );
+	}
+
+	/**
+	 * Tests ::get_all_settings.
+	 *
+	 * @covers ::get_all_settings
+	 */
+	public function test_get_all_settings_forced() {
+		$original = [
+			'foo'                       => 'bar',
+			Settings::INSTALLED_VERSION => self::VERSION,
+		];
+		$settings = [
+			'foo'                       => 'barfoo',
+			Settings::INSTALLED_VERSION => self::VERSION,
+		];
+
+		// Prepare state - settings have alreaddy been loaded.
+		$this->set_value( $this->sut, 'settings', $original );
+
+		$this->sut->shouldReceive( 'load_settings' )->once()->andReturn( $settings );
+
+		$this->assertSame( $settings, $this->sut->get_all_settings( true ) );
+	}
+
+	/**
+	 * Tests ::get_all_settings.
+	 *
+	 * @covers ::get_all_settings
+	 */
+	public function test_get_all_settings_no_version() {
+		$original = [
+			'foo' => 'bar',
+		];
+		$settings = [
+			'foo'                       => 'barfoo',
+			Settings::INSTALLED_VERSION => self::VERSION,
+		];
+
+		// Prepare state - settings have alreaddy been loaded.
+		$this->set_value( $this->sut, 'settings', $original );
+
+		$this->sut->shouldReceive( 'load_settings' )->once()->andReturn( $settings );
+
+		$this->assertSame( $settings, $this->sut->get_all_settings() );
+	}
+
+	/**
+	 * Tests ::get_all_settings.
+	 *
+	 * @covers ::get_all_settings
+	 */
+	public function test_get_all_settings_version_mismatch() {
+		$original = [
+			'foo'                       => 'bar',
+			Settings::INSTALLED_VERSION => '1.2.3',
+		];
+		$settings = [
+			'foo'                       => 'barfoo',
+			Settings::INSTALLED_VERSION => self::VERSION,
+		];
+
+		// Prepare state - settings have alreaddy been loaded.
+		$this->set_value( $this->sut, 'settings', $original );
+
+		$this->sut->shouldReceive( 'load_settings' )->once()->andReturn( $settings );
+
+		$this->assertSame( $settings, $this->sut->get_all_settings() );
+	}
+
+	/**
+	 * Tests ::load_settings.
+	 *
+	 * @covers ::load_settings
+	 */
+	public function test_load_settings() {
+		$setting1 = 'foo';
+		$setting2 = 'baz';
+		$settings = [
+			$setting1                   => 'barfoo',
+			Settings::INSTALLED_VERSION => '1.2.3',
+		];
+		$defaults = [
+			$setting1 => 'bar',
+			$setting2 => 'foobar',
+		];
+
+		$this->options->shouldReceive( 'get' )
+			->once()
+			->with( Options::OPTION )
+			->andReturn( $settings );
+
+		$this->sut->shouldReceive( 'get_defaults' )
+			->once()
+			->andReturn( $defaults );
+
+		$this->options->shouldReceive( 'set' )
+			->once()
+			->with( Options::OPTION, m::type( 'array' ) );
+
+		$result = $this->sut->load_settings();
+
+		$this->assert_is_array( $result );
+		$this->assertArrayHasKey( $setting1, $result );
+		$this->assertSame( 'barfoo', $result[ $setting1 ] );
+		$this->assertArrayHasKey( $setting2, $result );
+		$this->assertSame( 'foobar', $result[ $setting2 ] );
+		$this->assertArrayHasKey( Settings::INSTALLED_VERSION, $result );
+	}
+
+	/**
+	 * Tests ::load_settings.
+	 *
+	 * @covers ::load_settings
+	 */
+	public function test_load_settings_invalid_result() {
+		$defaults = [
+			'foo' => 'bar',
+			'baz' => 'foobar',
+		];
+
+		$this->options->shouldReceive( 'get' )
+			->once()
+			->with( Options::OPTION )
+			->andReturn( false );
+
+		$this->sut->shouldReceive( 'get_defaults' )
+			->once()
+			->andReturn( $defaults );
+
+		$this->options->shouldReceive( 'set' )
+			->once()
+			->with( Options::OPTION, m::type( 'array' ) );
+
+		$this->assertSame( $defaults, $this->sut->load_settings() );
+	}
+
+	/**
+	 * Tests ::load_settings.
+	 *
+	 * @covers ::load_settings
+	 */
+	public function test_load_settings_everything_in_order() {
+		$settings = [
+			'foo' => 'barfoo',
+			'baz' => 'foo',
+		];
+		$defaults = [
+			'foo' => 'bar',
+			'baz' => 'foobar',
+		];
+
+		$this->options->shouldReceive( 'get' )
+			->once()
+			->with( Options::OPTION )
+			->andReturn( $settings );
+
+		$this->sut->shouldReceive( 'get_defaults' )
+			->once()
+			->andReturn( $defaults );
+
+		$this->options->shouldReceive( 'set' )->never();
+
+		$this->assertSame( $settings, $this->sut->load_settings() );
+	}
+
+	/**
+	 * Tests ::get.
+	 *
+	 * @covers ::get
+	 */
+	public function test_get() {
+		$setting_key   = 'foo';
+		$setting_value = 'bar';
+		$settings      = [
+			$setting_key => $setting_value,
+			'something'  => 'else',
+		];
+
+		$this->sut->shouldReceive( 'get_all_settings' )->once()->andReturn( $settings );
+
+		$this->assertSame( $setting_value, $this->sut->get( $setting_key ) );
+	}
+
+	/**
+	 * Tests ::get.
+	 *
+	 * @covers ::get
+	 */
+	public function test_get_invalid_setting() {
+		$setting_key   = 'foo';
+		$setting_value = 'bar';
+		$settings      = [
+			$setting_key => $setting_value,
+			'something'  => 'else',
+		];
+
+		$this->sut->shouldReceive( 'get_all_settings' )->once()->andReturn( $settings );
+		$this->expect_exception( \UnexpectedValueException::class );
+
+		$this->assertNull( $this->sut->get( 'invalid setting' ) );
+	}
+
+	/**
+	 * Tests ::set.
+	 *
+	 * @covers ::set
+	 */
+	public function test_set() {
+		$setting_key   = 'my_key';
+		$setting_value = 'bar';
+
+		$orig_settings = [
+			'a_setting'  => 666,
+			$setting_key => 'some other value',
+			'something'  => 'else',
+		];
+
+		$new_settings                 = $orig_settings;
+		$new_settings[ $setting_key ] = $setting_value;
+
+		$this->sut->shouldReceive( 'get_all_settings' )->once()->andReturn( $orig_settings );
+		$this->options->shouldReceive( 'set' )->once()->with( Options::OPTION, $new_settings )->andReturn( true );
+
+		$this->assertTrue( $this->sut->set( $setting_key, $setting_value ) );
+		$this->assert_attribute_same( $new_settings, 'settings', $this->sut );
+	}
+
+	/**
+	 * Tests ::set.
+	 *
+	 * @covers ::set
+	 */
+	public function test_set_db_error() {
+		$setting_key   = 'my_key';
+		$setting_value = 'bar';
+		$orig_settings = [
+			'a_setting'  => 666,
+			$setting_key => 'some other value',
+			'something'  => 'else',
+		];
+
+		$new_settings                 = $orig_settings;
+		$new_settings[ $setting_key ] = $setting_value;
+
+		$cached_settings = $this->get_value( $this->sut, 'settings' );
+
+		$this->sut->shouldReceive( 'get_all_settings' )->once()->andReturn( $orig_settings );
+		$this->options->shouldReceive( 'set' )->once()->with( Options::OPTION, $new_settings )->andReturn( false );
+
+		$this->assertFalse( $this->sut->set( $setting_key, $setting_value ) );
+		$this->assert_attribute_same( $cached_settings, 'settings', $this->sut );
+	}
+
+	/**
+	 * Tests ::set.
+	 *
+	 * @covers ::set
+	 */
+	public function test_set_invalid_setting() {
+		$setting_key   = 'invalid_key';
+		$setting_value = 'bar';
+		$orig_settings = [
+			'a_setting' => 666,
+			'my_key'    => 'some other value',
+			'something' => 'else',
+		];
+
+		$cached_settings = $this->get_value( $this->sut, 'settings' );
+
+		$this->sut->shouldReceive( 'get_all_settings' )->once()->andReturn( $orig_settings );
+
+		$this->expect_exception( \UnexpectedValueException::class );
+
+		$this->options->shouldReceive( 'set' )->never();
+
+		$this->assertNull( $this->sut->set( $setting_key, $setting_value ) );
+		$this->assert_attribute_same( $cached_settings, 'settings', $this->sut );
+	}
+
+	/**
+	 * Tests ::get_fields.
+	 *
+	 * @covers ::get_fields
+	 */
+	public function test_get_fields() {
+		$site_name = 'My Cool Site';
+
+		Functions\expect( 'get_bloginfo' )->once()->with( 'name', 'display' )->andReturn( $site_name );
+
+		$result = $this->sut->get_fields();
+
+		$this->assert_is_array( $result );
+		$this->assertContainsOnly( 'string', \array_keys( $result ) );
+		$this->assertContainsOnly( 'array', $result );
+	}
+
+	/**
+	 * Tests ::get_defaults.
+	 *
+	 * @covers ::get_defaults
+	 *
+	 * @uses ::get_fields
+	 */
+	public function test_get_defaults() {
+		$site_name = 'My Cool Site';
+
+		Functions\expect( 'get_bloginfo' )->once()->with( 'name', 'display' )->andReturn( $site_name );
+
+		$result = $this->sut->get_defaults();
+
+		$this->assert_is_array( $result );
+		$this->assertNotContainsOnly( 'array', $result );
+		$this->assertSame( '', $result[ Settings::INSTALLED_VERSION ] );
+	}
+}
