30ae850a620b12e0ff63acf3d6b640e5e7b21eed
mundschenk_at <github@mundschenk.at>
2021-06-29 22:28:21 +0200

    Use custom Factory_Exception instead of RuntimeException

---
+++ includes/media-credit/exceptions/class-factory-exception.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of Media Credit.
+ *
+ * Copyright 2021 Peter Putzer.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  ***
+ *
+ * @package mundschenk-at/media-credit
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Media_Credit\Exceptions;
+
+/**
+ * An exception indicating a problem with the object factory.
+ *
+ * @since 4.2.0
+ *
+ * @author Peter Putzer <github@mundschenk.at>
+ */
+class Factory_Exception extends \RuntimeException {
+}
